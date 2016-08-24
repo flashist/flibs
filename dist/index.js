6cfc35010271778e6e94b10a4a53d55148846b21
Mark Dolbyrev <mark.dolbyrev@lcsas-MacBook-Pro-2.local>
2016-08-24 22:45:47 +0300

    Move some settings (color, size, e.t.c.) into a config file.

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,9 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 __export(require('./TestLibClass'));
+__export(require('./other/dragHelper/DragHelper'));
+__export(require('./other/dragHelper/DragHelperEvent'));
 __export(require('./manager/input/InputManager'));
 __export(require('./manager/input/InputManagerEvent'));
 __export(require('./manager/input/InputManagerEventData'));
-__export(require('./other/dragHelper/DragHelper'));
-__export(require('./other/dragHelper/DragHelperEvent'));
 //# sourceMappingURL=index.js.map
