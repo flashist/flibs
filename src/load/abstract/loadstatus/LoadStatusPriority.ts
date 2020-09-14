7743a352906ae80156cd3d46cf7c179cf0632981
Mark Dolbyrev <mark.dolbyrev@skywindgroup.com>
2020-09-14 14:54:06 +0300

    In progress.

--- src/load/abstract/loadstatus/LoadStatusPriority.ts
+++ src/load/abstract/loadstatus/LoadStatusPriority.ts
@@ -3,10 +3,10 @@ import {LoadStatus} from "./LoadStatus";
 export const LoadStatusPriority = ((): {[key: string]: number} => {
     const result: {[key: string]: number} = {};
 
-    result[LoadStatus.ERROR] = 0;
-    result[LoadStatus.WAIT] = 1;
-    result[LoadStatus.LOADING] = 2;
-    result[LoadStatus.COMPLETE] = 3;
+    result[LoadStatus.ERROR] = 1000;
+    result[LoadStatus.LOADING] = 100;
+    result[LoadStatus.WAIT] = 10;
+    result[LoadStatus.COMPLETE] = 0;
 
     return result;
 })();
