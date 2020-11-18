5b30f1b6ffe384f064dc4a6f9ac9fbc01990be4b
Mark Dolbyrev <mark.dolbyrev@skywindgroup.com>
2020-11-18 14:27:03 +0300

    Libs update.

--- src/base/data/BaseDataVO.ts
+++ src/base/data/BaseDataVO.ts
@@ -8,6 +8,9 @@ export class BaseDataVO extends BaseEventDispatcher implements IGenericObjectVO 
     public type: string = "";
     public id: string = "";
 
+    protected explicitSourcePropertyNames: (keyof this)[];
+    protected ignoreSourcePropertyNames: (keyof this)[];
+
     update(source: Partial<this>): void {
         const isChanged: boolean = ObjectTools.copyProps(this, source);
 
@@ -16,4 +19,44 @@ export class BaseDataVO extends BaseEventDispatcher implements IGenericObjectVO 
         }
     }
 
+    public get sourceJsonObj(): Partial<this> {
+        let sourceObj: Partial<this> = {};
+
+        if (this.explicitSourcePropertyNames || this.ignoreSourcePropertyNames) {
+            for (let keyName in this) {
+                let isIgnored: boolean = false;
+                if (this.ignoreSourcePropertyNames) {
+                    if (this.ignoreSourcePropertyNames.indexOf(keyName) !== -1) {
+                        isIgnored = true;
+                    }
+                }
+
+                let isNeedToCopy: boolean = false;
+                if (!isIgnored) {
+                    if (this.explicitSourcePropertyNames) {
+                        if (this.explicitSourcePropertyNames.indexOf(keyName) !== -1) {
+                            isNeedToCopy = true;
+                        }
+
+                    } else {
+                        isNeedToCopy = true;
+                    }
+                }
+
+                if (isNeedToCopy) {
+                    ObjectTools.copySingleProp(
+                        sourceObj,
+                        this,
+                        keyName
+                    );
+                }
+            }
+        }
+
+        return sourceObj;
+    }
+
+    public get sourceJsonText(): string {
+        return JSON.stringify(this.sourceJsonObj);
+    }
 }
