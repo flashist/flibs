a3a2eb939aa3092c387ed02041b9a2a839a3c4ea
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2022-11-14 16:17:57 +0300

    Remove of confusing logic

--- src/model/generic/IGenericObjectWithStaticVO.ts
+++
@@ -1,9 +0,0 @@
-import {IGenericObjectVO} from "./IGenericObjectVO";
-
-export interface IGenericObjectWithStaticVO<StaticDataType extends IGenericObjectVO = IGenericObjectVO>
-    extends IGenericObjectVO {
-
-    staticId?: string;
-    staticType?: string;
-    staticData?: StaticDataType;
-}
