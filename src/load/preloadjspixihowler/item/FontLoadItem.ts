1694be82dd4d6cd8dff41bf7efba32841a19e9c8
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2022-12-15 18:08:34 +0300

    Update

--- src/load/preloadjspixihowler/item/FontLoadItem.ts
+++ src/load/preloadjspixihowler/item/FontLoadItem.ts
@@ -18,7 +18,6 @@ export class FontLoadItem<DataType extends any = any> extends AbstractLoadItem<D
         style.type = "text/css";
         let fontRule: string = this.generateFontFaceRule();
         style.sheet.insertRule(fontRule);
-        // style.innerHTML = fontRule;
         //
         document.getElementsByTagName('head')[0].appendChild(style);
 
@@ -47,16 +46,29 @@ export class FontLoadItem<DataType extends any = any> extends AbstractLoadItem<D
         
         this.fontLoadingConfig.fontloading = (familyName: string, fontValidationDescription: string) => {
             console.log("FontLoadItem | fontloading __ familyName: ", familyName);
+
+            // THIS IS MAGIC!
+            // We're creating virtually invisible p tag in order to force browsers
+            // to prepare the font for later usage
+            var el = document.createElement('p');
+            el.style.fontFamily = this.config.fontFace["font-family"];
+            el.style.fontWeight = this.config.fontFace["font-weight"];
+            el.style.fontStyle = this.config.fontFace["font-style"];
+            el.style.fontSize = "0px";
+            el.style.visibility = "hidden";
+            el.innerHTML = '.';
+            //
+            document.body.appendChild(el);
         };
         this.fontLoadingConfig.fontactive = (familyName: string, fontValidationDescription: string) => {
             console.log("FontLoadItem | fontactive __ familyName: ", familyName);
-            /*setTimeout(
+            // Give browser some time to prepare fonts for using
+            setTimeout(
                 () => {
-                    this.processLoadingComplete(familyName);
+                    this.processLoadingComplete(familyName, familyName);
                 },
-                5000
-            );*/
-            this.processLoadingComplete(familyName, familyName);
+                500
+            );
         };
         this.fontLoadingConfig.fontinactive = (familyName: string, fontValidationDescription: string) => {
             console.log("FontLoadItem | fontinactive __ familyName: ", familyName);
@@ -76,12 +88,15 @@ export class FontLoadItem<DataType extends any = any> extends AbstractLoadItem<D
             filepathWithoutExtension = filepathWithoutExtension.substring(0, lastDotIndex);
         }
 
+        // The text after the last slash is the filename without extension
+        let fileNameWithoutExtension: string = filepathWithoutExtension.split("/").pop();
+
         let result: string = `@font-face {
             src: url('${filepathWithoutExtension}.eot');
             src: url('${filepathWithoutExtension}.woff2') format('woff2'),
                 url('${filepathWithoutExtension}.woff') format('woff'),
                 url('${filepathWithoutExtension}.ttf') format('truetype'),
-                url('${filepathWithoutExtension}.svg#${filepathWithoutExtension}') format('svg'),
+                url('${filepathWithoutExtension}.svg#${fileNameWithoutExtension}') format('svg'),
                 url('${filepathWithoutExtension}.eot?#iefix') format('embedded-opentype');`;
 
         for (let propName in this.config.fontFace) {
