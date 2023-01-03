e5def736b5d9442e1597c30623db5345d28f45eb
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2023-01-03 12:08:09 +0300

    Update

--- src/servicelocator/ServiceLocator.ts
+++ src/servicelocator/ServiceLocator.ts
@@ -84,7 +84,7 @@ export class ServiceLocator {
         }*/
     }
 
-    static getInstance<Type extends any>(item: IConstructor<Type>, ...args: ConstructorParameters<IConstructor<Type>>): Type
+    static getInstance<Type extends IConstructor>(item: Type, ...args: ConstructorParameters<Type>): Type;
     static getInstance<Type extends any>(item: IConstructor<Type>, ...args): Type {
 
         let result: Type;
