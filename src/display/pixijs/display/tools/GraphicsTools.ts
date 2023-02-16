64e6f947a4fa132f93347ba4ef511183713524f7
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2023-02-16 23:14:07 +0300

    Update

--- src/display/pixijs/display/tools/GraphicsTools.ts
+++ src/display/pixijs/display/tools/GraphicsTools.ts
@@ -1,7 +1,7 @@
 import {Graphics} from "../../../../index";
 
 export class GraphicsTools {
-    static createTraspRect(x?: number = 0, y?: number = 0, width?: number = 10, height?: number = 10): Graphics {
+    static createTraspRect(x: number = 0, y: number = 0, width: number = 10, height: number = 10): Graphics {
         const result: Graphics = new Graphics();
         result.beginFill(0x000000, 0.001);
         result.drawRect(x, y, width, height);
