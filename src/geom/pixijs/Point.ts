65bd24c668159db532618b77959687d11f78c5d1
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2023-03-10 13:46:59 +0300

    Update

--- src/geom/pixijs/Point.ts
+++ src/geom/pixijs/Point.ts
@@ -1,29 +1,5 @@
-import {Point as PixiPoint} from "pixi.js";
+import {Point} from "pixi.js";
 // import Point = PIXI.Point;
-export class Point extends PixiPoint {
-    subtract(point: Point): Point {
-        return new Point(
-        this.x - point.x,
-        this.y - point.y
-        )
-    }
-
-    add(point: Point): Point {
-        return new Point(
-            this.x + point.x,
-            this.y + point.y
-        )
-    }
-
-    normalize(length: number = 1): void {
-        var magnitude: number = Math.sqrt(this.x * this.x + this.y * this.y);
-        if (magnitude != 0) {
-            this.x *= length / magnitude;
-            this.y *= length / magnitude;
-        }
-    }
-
-    getLength(): number {
-        return Math.sqrt(this.x * this.x + this.y * this.y);
-    }
+export {
+    Point
 };
