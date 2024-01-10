35ff74ea7050e6117a3b633be564b9621931797a
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2024-01-10 12:41:28 +0300

    Update

--- src/html/tools/HtmlTools.ts
+++ src/html/tools/HtmlTools.ts
@@ -17,4 +17,26 @@ export class HtmlTools {
         // Change this to div.childNodes to support multiple top-level nodes.
         return div.firstChild;
     }
+
+    static checkIfElementIsParentOf(element: Node, targetParent: Node, maxIterations: number = 50): boolean {
+        let result: boolean = false;
+
+        let curIteraction: number = 0;
+        let curParent: Node = element.parentNode;
+        while (curParent) {
+            if (curParent === targetParent) {
+                result = true;
+                break;
+            }
+
+            curParent = curParent.parentElement;
+
+            curIteraction++;
+            if (curIteraction >= maxIterations) {
+                break;
+            }
+        }
+
+        return result;
+    }
 }
