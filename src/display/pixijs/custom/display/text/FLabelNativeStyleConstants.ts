4ea72a9954f53914e8a75ba59b4aeaad4bb823c7
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2025-04-11 14:40:50 +0300

    Update

--- src/display/pixijs/custom/display/text/FLabelNativeStyleConstants.ts
+++ src/display/pixijs/custom/display/text/FLabelNativeStyleConstants.ts
@@ -3,8 +3,10 @@ import { Align } from "../../../../../other";
 export enum FLabelNativeStyleFontWeight {
     NORMAL = 'normal',
     BOLD = 'bold',
+
     BOLDER = 'bolder',
     LIGHTER = 'lighter',
+
     WEIGHT_100 = '100',
     WEIGHT_200 = '200',
     WEIGHT_300 = '300',
@@ -13,7 +15,16 @@ export enum FLabelNativeStyleFontWeight {
     WEIGHT_600 = '600',
     WEIGHT_700 = '700',
     WEIGHT_800 = '800',
-    WEIGHT_900 = '900'
+    WEIGHT_900 = '900',
+
+    THIN = WEIGHT_100,
+    EXTRA_LIGHT = WEIGHT_200,
+    LIGHT = WEIGHT_300,
+    REGULAR = WEIGHT_400,
+    MEDIUM = WEIGHT_500,
+    SEMI_BOLD = WEIGHT_600,
+    EXTRA_BOLD = WEIGHT_800,
+    BLACK = WEIGHT_900,
 };
 
 export enum FLabelNativeStyleAlign {
